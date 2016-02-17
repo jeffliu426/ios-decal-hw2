9b2de7e2e10136f49cfcf4f84ff85f44c59e182f
Jeffrey Liu <jliu23@berkeley.edu>
2016-02-16 21:43:19 -0800

    made functioning keyboard that is on right side of screen for one handed texting

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -9,8 +9,13 @@
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
+    
 
     @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet var deleteKeyboardButton: UIButton!
+    @IBOutlet var returnKeyboardButton: UIButton!
+    @IBOutlet var aKeyboardButton: UIButton!
+    
     
     var keyboardView: UIView!
 
@@ -37,6 +42,21 @@ class KeyboardViewController: UIInputViewController {
     override func textDidChange(textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
     }
+    
+    @IBAction func writeA(sender: AnyObject) {
+        if let button = sender as? UIButton, text = button.titleLabel?.text {
+            self.textDocumentProxy.insertText(text)
+        }
+    }
+    
+    @IBAction func returnPressed() {
+        self.textDocumentProxy.insertText("\n")
+    }
+    
+    @IBAction func deletePressed() {
+        self.textDocumentProxy.deleteBackward()
+    }
+    
 
     func loadInterface() {
         let keyboardNib = UINib(nibName: "Keyboard", bundle: nil)
@@ -46,7 +66,26 @@ class KeyboardViewController: UIInputViewController {
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
         
+        returnKeyboardButton.addTarget(self, action: "returnPressed", forControlEvents: .TouchUpInside)
+        aKeyboardButton.addTarget(self, action: "writeA:", forControlEvents: .TouchUpInside)
+        
+        deleteKeyboardButton.addTarget(self, action: "deletePressed", forControlEvents: .TouchUpInside)
+        
+        
     }
 
+}
 
+
+extension CALayer {
+    var borderUIColor: UIColor {
+        set {
+            self.borderColor = newValue.CGColor
+            self.borderWidth = 2
+        }
+        
+        get {
+            return UIColor(CGColor: self.borderColor!)
+        }
+    }
 }
